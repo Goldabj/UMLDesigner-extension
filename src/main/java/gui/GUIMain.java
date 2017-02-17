3cf234f559b547fef9107c69ca680783ae97d2b0
Brandon-Benedict <benedib1@rose-hulman.edu>
2017-02-16 22:54:05 -0500

    fixing gradle stuff is really fun... not

--- src/main/java/gui/GUIMain.java
+++ src/main/java/gui/GUIMain.java
@@ -1,10 +1,29 @@
 package gui;
 
-public class GUIMain {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GUIMain extends Application{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Button allAsbtn = new Button("Click this badass button to give"
+				+ " BBV squad all As on this not as cool project");	
+		
+		
+		StackPane root = new StackPane();
+		root.getChildren().add(allAsbtn);
+		Scene scene1 = new Scene(root, 500, 500);
+		primaryStage.setScene(scene1);
+		primaryStage.show();
 	}
 
 }
